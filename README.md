b487d803a8fc696e64b3fd9d0e252c9bf7fc0e18
Daniel Du <danghoangnhan.1@gmail.com>
2023-07-23 01:48:49 +0800

    recode vgg 16 and vgg 19

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# My Personal  machine learning Library
+# Tensorflow model Library
 
-This is my personal library that implements some common machine learning models.
+This is my personal library that implements  state-of-the-art deeplearning models. The models are implemented using TensorFlow and Keras.
 
 ## Requirements
 
 - ![Conda](https://img.shields.io/badge/Conda-4.13.0-brightgreen)
 - [![TensorFlow](https://img.shields.io/badge/TensorFlow-2.8.0-blue?logo=tensorflow)](https://tensorflow.org/)
 
-# Installation
+## Installation
 
 1. Clone the repository:
 
@@ -15,6 +15,17 @@ This is my personal library that implements some common machine learning models.
    git clone https://github.com/danghoangnhan/machineLearning
    cd machineLearning
    ```
+## References
 
+### VGG
+- VGG-16 Architecture Paper:
+    - Title: Very Deep Convolutional Networks for Large-Scale Image Recognition
+    - Authors: Karen Simonyan, Andrew Zisserman
+    - Year: 2014
+    - [Link to the Paper](https://arxiv.org/abs/1409.1556)
 
-2. enjoy :)
+- VGG-19 Architecture Paper:
+    - Title: Very Deep Convolutional Networks for Large-Scale Image Recognition
+    - Authors: Karen Simonyan, Andrew Zisserman
+    - Year: 2014
+    - [Link to the Paper](https://arxiv.org/abs/1409.1556)
--- model/CNN/VGGNet.py
+++ model/CNN/VGGNet.py
@@ -1,133 +1,80 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras import backend as K
+from tensorflow.keras import Sequential
 #source: https://www.kaggle.com/code/blurredmachine/vggnet-16-architecture-a-complete-guide?scriptVersionId=39674893
-class VGG_16(Model):
-    def __init__(self):
-        super(VGG_16, self).__init__()
-        self.conv1 = Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv2 = Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool1 = MaxPooling2D((2, 2))
-        
-        self.conv3 = Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv4 = Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool2 = MaxPooling2D((2, 2))
-        
-        self.conv5 = Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv6 = Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv7 = Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool3 = MaxPooling2D((2, 2))
-        
-        self.conv8 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv9 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv10 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool4 = MaxPooling2D((2, 2))
-        
-        self.conv11 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv12 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv13 = Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool5 = MaxPooling2D((2, 2))
-        
-        self.flatten = Flatten()
-        self.dense1 = Dense(4096, activation="relu")
-        self.dense2 = Dense(4096, activation="relu")
-        self.output_layer = Dense(1000, activation="softmax")
-    
-    def call(self, inputs):
-        x = self.conv1(inputs)
-        x = self.conv2(x)
-        x = self.pool1(x)
-        
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.pool2(x)
-        
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
-        x = self.pool3(x)
-        
-        x = self.conv8(x)
-        x = self.conv9(x)
-        x = self.conv10(x)
-        x = self.pool4(x)
-        
-        x = self.conv11(x)
-        x = self.conv12(x)
-        x = self.conv13(x)
-        x = self.pool5(x)
-        
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        output = self.output_layer(x)
-        
-        return output
+class VGG16(Sequential):
+    def __init__(self,width, height, depth, classes=1000):
+        super(VGG16, self).__init__()
+        inputShape = (height, width, depth)
 
-class VGG_19(Model):
-    def __init__(self, classes=1000):
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            
+        self.add(Conv2D(inputShape=inputShape,filters =64, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
+
+        self.add(Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
+
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
+
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
+
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
+
+        self.add(Flatten())
+        self.add(Dense(4096, activation="relu"))
+        self.add(Dense(4096, activation="relu"))
+        self.add(Dense(classes, activation="softmax"))
+
+class VGG_19(Sequential):
+    def __init__(self, width, height, depth, classes=1000):
         super(VGG_19, self).__init__()
 
-        self.conv1_1 = Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv1_2 = Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool1 = MaxPooling2D((2, 2))
+        input_shape = (height, width, depth)
 
-        self.conv2_1 = Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv2_2 = Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool2 = MaxPooling2D((2, 2))
+        if tf.keras.backend.image_data_format() == "channels_first":
+            input_shape = (depth, height, width)
 
-        self.conv3_1 = Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv3_2 = Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv3_3 = Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv3_4 = Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool3 = MaxPooling2D((2, 2))
+        self.add(Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu", input_shape=input_shape))
+        self.add(Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
 
-        self.conv4_1 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv4_2 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv4_3 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv4_4 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool4 = MaxPooling2D((2, 2))
+        self.add(Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(128, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
 
-        self.conv5_1 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv5_2 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv5_3 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.conv5_4 = Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu")
-        self.pool5 = MaxPooling2D((2, 2))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(256, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
 
-        self.flatten = Flatten()
-        self.dense1 = Dense(4096, activation="relu")
-        self.dense2 = Dense(4096, activation="relu")
-        self.output_layer = Dense(classes, activation="softmax")
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
 
-    def call(self, inputs):
-        x = self.conv1_1(inputs)
-        x = self.conv1_2(x)
-        x = self.pool1(x)
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(Conv2D(512, kernel_size=(3, 3), padding="same", activation="relu"))
+        self.add(MaxPooling2D((2, 2)))
 
-        x = self.conv2_1(x)
-        x = self.conv2_2(x)
-        x = self.pool2(x)
-
-        x = self.conv3_1(x)
-        x = self.conv3_2(x)
-        x = self.conv3_3(x)
-        x = self.conv3_4(x)
-        x = self.pool3(x)
-
-        x = self.conv4_1(x)
-        x = self.conv4_2(x)
-        x = self.conv4_3(x)
-        x = self.conv4_4(x)
-        x = self.pool4(x)
-
-        x = self.conv5_1(x)
-        x = self.conv5_2(x)
-        x = self.conv5_3(x)
-        x = self.conv5_4(x)
-        x = self.pool5(x)
-
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        output = self.output_layer(x)
-
-        return output
+        self.add(Flatten())
+        self.add(Dense(4096, activation="relu"))
+        self.add(Dense(4096, activation="relu"))
+        self.add(Dense(classes, activation="softmax"))
